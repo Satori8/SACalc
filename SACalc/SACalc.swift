61eac6cfe73361b922f3ce6ddf3e22c293abeb42
Александр <aleksandr@mac.local>
2015-08-21 05:50:51 +0300

    outlets

---
+++ SACalc/SACalc.swift
@@ -0,0 +1,20 @@
+//
+//  SACalc.swift
+//  SACalc
+//
+//  Created by Александр on 21.08.15.
+//  Copyright (c) 2015 Satori. All rights reserved.
+//
+
+import UIKit
+
+class SACalc: NSObject {
+    var number1: Double?
+    var number2: Double?
+    
+    func equals() -> Double{
+        var result:Double?
+        return result!
+    }
+   
+}
--- SACalc/ViewController.swift
+++ SACalc/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var calcField: UITextField!
+    
+    @IBAction func digitAction(sender: UIButton) {
+    }
+    
+    @IBAction func operationAction(sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
